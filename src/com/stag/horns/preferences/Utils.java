60c6b25dce35b17a20907a572d1b5af7a8fe2825
Roger Truttmann <roger.truttmann@gmx.ch>
2017-09-21 00:26:17 +0800

    New Personalization icons

--- src/com/stag/horns/preferences/Utils.java
+++ src/com/stag/horns/preferences/Utils.java
@@ -14,7 +14,7 @@
  * under the License.
  */
 
-package com.kcuf.settings.preferences;
+package com.stag.horns.preferences;
 
 import android.app.Activity;
 import android.content.Context;
@@ -39,7 +39,7 @@ import android.view.Surface;
 import android.view.WindowManager;
 
 public final class Utils {
-    private static final String TAG = "KCUFSettingsUtils";
+    private static final String TAG = "HornsUtils";
 
     // Device types
     private static final int DEVICE_PHONE = 0;
