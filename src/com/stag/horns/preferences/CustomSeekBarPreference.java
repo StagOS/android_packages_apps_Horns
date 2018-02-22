a0a40e4118d5b31bc8552c688eccb24a34a0881c
xyyx <xyyx@mail.ru>
2018-02-22 15:20:18 +0800

    CustomSeekBar: Fix disabling when create before: https://imgur.com/a/jtNip after: https://imgur.com/a/Q1rJH

--- src/com/stag/horns/preferences/CustomSeekBarPreference.java
+++ src/com/stag/horns/preferences/CustomSeekBarPreference.java
@@ -146,6 +146,8 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
 
         view.setDividerAllowedAbove(false);
         //view.setDividerAllowedBelow(false);
+
+        mSeekBar.setEnabled(isEnabled());
     }
 
     public void setMax(int max) {
