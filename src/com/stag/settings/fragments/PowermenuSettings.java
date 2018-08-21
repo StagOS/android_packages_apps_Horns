c7560a0bf222eeee8877f80df7379a5cde26408f
Vol Zhdanov <wight554@gmail.com>
2018-08-21 12:18:00 +0000

    PowerMenuSettings: Hide user related switches when no users added

--- src/com/stag/settings/fragments/PowermenuSettings.java
+++ src/com/stag/settings/fragments/PowermenuSettings.java
@@ -19,7 +19,10 @@ package com.stag.settings.fragments;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.res.Resources;
+import android.content.pm.UserInfo;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.UserManager;
 import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
@@ -41,9 +44,15 @@ import com.stag.settings.preferences.Utils;
 public class PowermenuSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private static final String KEY_POWERMENU_LOGOUT = "powermenu_logout";
     private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
+    private static final String KEY_POWERMENU_USERS = "powermenu_users";
 
+    private SwitchPreference mPowermenuLogout;
     private SwitchPreference mPowermenuTorch;
+    private SwitchPreference mPowermenuUsers;
+
+    private UserManager mUserManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,6 +68,20 @@ public class PowermenuSettings extends SettingsPreferenceFragment implements
         } else {
         mPowermenuTorch.setChecked((Settings.System.getInt(resolver,
                 Settings.System.POWERMENU_TORCH, 0) == 1));
+        }
+
+        mPowermenuLogout = (SwitchPreference) findPreference(KEY_POWERMENU_LOGOUT);
+        mPowermenuLogout.setOnPreferenceChangeListener(this);
+        mPowermenuUsers = (SwitchPreference) findPreference(KEY_POWERMENU_USERS);
+        mPowermenuUsers.setOnPreferenceChangeListener(this);
+        if (!mUserManager.supportsMultipleUsers()) {
+            prefScreen.removePreference(mPowermenuLogout);
+            prefScreen.removePreference(mPowermenuUsers);
+        } else {
+            mPowermenuLogout.setChecked((Settings.System.getInt(resolver,
+                    Settings.System.POWERMENU_LOGOUT, 0) == 1));
+            mPowermenuUsers.setChecked((Settings.System.getInt(resolver,
+                    Settings.System.POWERMENU_USERS, 0) == 1));
         }
     }
 
@@ -78,10 +101,20 @@ public class PowermenuSettings extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mPowermenuTorch) {
+        if (preference == mPowermenuLogout) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LOGOUT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPowermenuTorch) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWERMENU_TORCH, value ? 1 : 0);
+            return true;
+        } else if (preference == mPowermenuUsers) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_USERS, value ? 1 : 0);
             return true;
         }
         return false;
