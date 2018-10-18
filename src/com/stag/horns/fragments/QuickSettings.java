59fd7bda85a9fef2f293d9b1a63efd9849efd13e
calebcabob <calphonic@gmail.com>
2018-10-18 16:22:47 +0000

    Introduce QS tile style picker [2/3]
    Bootleggers edit: Moved to our dumpster

--- src/com/stag/horns/fragments/QuickSettings.java
+++ src/com/stag/horns/fragments/QuickSettings.java
@@ -42,12 +42,23 @@ import com.stag.horns.preferences.CustomSeekBarPreference;
 public class QuickSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private ListPreference mQSTileStyle;
+    private static final String QS_TILE_STYLE = "qs_tile_style";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.horns_quicksettings);
         ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        // set qs tile style
+        mQSTileStyle = (ListPreference) findPreference(QS_TILE_STYLE);
+        int style = Settings.System.getInt(resolver,
+                Settings.System.QS_TILE_STYLE, 0);
+        mQSTileStyle.setValue(String.valueOf(style));
+        mQSTileStyle.setSummary(mQSTileStyle.getEntry());
+        mQSTileStyle.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -66,6 +77,14 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mQSTileStyle) {
+            int style = Integer.valueOf((String) newValue);
+            int index = mQSTileStyle.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.QS_TILE_STYLE, style);
+            mQSTileStyle.setSummary(mQSTileStyle.getEntries()[index]);
+            return true;
+        }
         return false;
     }
 }
