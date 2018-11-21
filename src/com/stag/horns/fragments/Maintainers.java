434265a3eca6855f4aaee34f091131d7fac2c148
vjspranav <pranavasri@live.in>
2018-11-21 18:44:49 +0100

    Show Maintainers list

--- src/com/stag/horns/fragments/Maintainers.java
+++ src/com/stag/horns/fragments/Maintainers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 StagOS Project
+ * Copyright (C) 2017 ColtOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,60 +15,23 @@
  */
 package com.stag.horns.fragments;
 
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+public class Maintainers extends SettingsPreferenceFragment {
 
-public class Maintainers extends Fragment {
-     private static final String MAINTAINERS_PATH = "/system/etc/Maintainers.txt";
-     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-        InputStreamReader inputReader = null;
-        String text = null;
- 	setHasOptionsMenu(true);
-         try {
-            StringBuilder data = new StringBuilder();
-            char tmp[] = new char[2048];
-            int numRead;
-             inputReader = new FileReader(MAINTAINERS_PATH);
-            while ((numRead = inputReader.read(tmp)) >= 0) {
-                data.append(tmp, 0, numRead);
-            }
-            text = data.toString();
-        } catch (IOException e) {
-            text = getString(R.string.maintainers_stag_error);
-        } finally {
-            try {
-                if (inputReader != null) {
-                    inputReader.close();
-                }
-            } catch (IOException e) {
-            }
-        }
-         final TextView textView = new TextView(getActivity());
-        textView.setText(text);
-         final ScrollView scrollView = new ScrollView(getActivity());
-        scrollView.addView(textView);
-         return scrollView;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.device_maintainers);
     }
- 	@Override
-       public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-           getActivity().onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
- }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.HORNS;
+     }
+}
