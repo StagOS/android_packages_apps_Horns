67c75b5cd82f2fdee0a7f155869225938744d566
Pranav Vashi <neobuddy89@gmail.com>
2019-05-25 14:26:39 +0200

    Lockscreen Visualizer: Add pulse magic [2/2]

--- src/com/stag/horns/fragments/LockScreenUi.java
+++ src/com/stag/horns/fragments/LockScreenUi.java
@@ -49,6 +49,7 @@ public class LockScreenUi extends SettingsPreferenceFragment implements
     private static final String LOCK_OWNERINFO_FONTS = "lock_ownerinfo_fonts";
     private static final String LOCKOWNER_FONT_SIZE = "lockowner_font_size";
     private static final String LOCK_SCREEN_VISUALIZER_CUSTOM_COLOR = "lock_screen_visualizer_custom_color";
+    private static final String KEY_LAVALAMP = "lockscreen_lavalamp_enabled";
 
     ListPreference mLockClockFonts;
     ListPreference mLockDateFonts;
@@ -57,6 +58,7 @@ public class LockScreenUi extends SettingsPreferenceFragment implements
     private CustomSeekBarPreference mDateFontSize;
     private CustomSeekBarPreference mOwnerInfoFontSize;
     private ColorPickerPreference mVisualizerColor;
+    private SwitchPreference mLavaLamp;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -115,6 +117,10 @@ public class LockScreenUi extends SettingsPreferenceFragment implements
         mVisualizerColor.setNewPreviewColor(visColor);
         mVisualizerColor.setAlphaSliderEnabled(true);
         mVisualizerColor.setOnPreferenceChangeListener(this);
+
+        // Lockscreen Visualizer pulse magic
+        mLavaLamp = (SwitchPreference) findPreference(KEY_LAVALAMP);
+        mLavaLamp.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -160,6 +166,10 @@ public class LockScreenUi extends SettingsPreferenceFragment implements
             Settings.System.putInt(resolver,
                     Settings.System.LOCK_SCREEN_VISUALIZER_CUSTOM_COLOR, intHex);
             preference.setSummary(hex);
+        } else if (preference == mLavaLamp) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(resolver,
+                Settings.Secure.LOCKSCREEN_LAVALAMP_ENABLED, value ? 1 : 0);
             return true;
         }
         return false;
