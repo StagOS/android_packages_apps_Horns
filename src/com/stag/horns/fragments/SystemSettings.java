af847743ad0a475d52388c5830a3dc4db27fb09c
Kshitij Gupta <kshitijgm@gmail.com>
2019-06-02 13:14:05 +0200

    Horns:: Introduce RGB accenter [2/2] *Add new System tab *drawable taken from https://gerrit.omnirom.org/#/c/android_packages_apps_OmniGears/+/29692/1/res/drawable/ic_settings_system.xml (From Omni) Adapt to 10 code

---
+++ src/com/stag/horns/fragments/SystemSettings.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2018 StagOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.stag.horns.fragments;
+
+import android.app.ActivityManager;
+import android.app.IActivityManager;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Process;
+import android.os.ServiceManager;
+import android.os.UserHandle;
+import android.os.SystemProperties;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import androidx.preference.SwitchPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.SettingsPreferenceFragment;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import com.stag.horns.preferences.CustomSeekBarPreference;
+import com.stag.horns.preferences.SecureSettingSwitchPreference;
+import com.stag.horns.preferences.SystemSettingSwitchPreference;
+
+import com.stag.horns.R;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class SystemSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "System";
+    private static final String ACCENT_COLOR = "accent_color";
+    private static final String ACCENT_COLOR_PROP = "persist.sys.theme.accentcolor";
+
+    private Handler mHandler;
+
+    private ColorPickerPreference mThemeColor;
+//    private Fragment mCurrentFragment = this;
+    private IOverlayManager mOverlayService;
+    private PackageManager mPackageManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.horns_system);
+        mOverlayService = IOverlayManager.Stub.asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
+        mPackageManager = getActivity().getPackageManager();
+	mHandler = new Handler();
+        ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        setupAccentPref();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.HORNS;
+    }
+
+    private void setupAccentPref() {
+        mThemeColor = (ColorPickerPreference) findPreference(ACCENT_COLOR);
+        String colorVal = SystemProperties.get(ACCENT_COLOR_PROP, "-1");
+        int color = "-1".equals(colorVal)
+                ? Color.WHITE
+                : Color.parseColor("#" + colorVal);
+        mThemeColor.setNewPreviewColor(color);
+        mThemeColor.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+	if (preference == mThemeColor) {
+            int color = (Integer) newValue;
+            String hexColor = String.format("%08X", (0xFFFFFFFF & color));
+            SystemProperties.set(ACCENT_COLOR_PROP, hexColor);
+	    try{
+            mOverlayService.reloadAndroidAssets(UserHandle.USER_CURRENT);
+            mOverlayService.reloadAssets("com.android.settings", UserHandle.USER_CURRENT);
+            mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT); 
+	    }catch(Exception ex){
+            }
+       return true;
+	}
+       return false;
+    }
+
+    private boolean isTheme(OverlayInfo oi) {
+        if (!OverlayInfo.CATEGORY_THEME.equals(oi.category)) {
+            return false;
+        }
+        try {
+            PackageInfo pi = mPackageManager.getPackageInfo(oi.packageName, 0);
+            return pi != null && !pi.isStaticOverlayPackage();
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+}
