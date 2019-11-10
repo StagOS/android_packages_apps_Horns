950285cf4d6f2af74987518c7c641599485f30d9
ezio84 <brabus84@gmail.com>
2019-11-10 13:40:15 +0100

    Add Led Light settings *Also add master settingswitch

--- src/com/stag/horns/fragments/NotificationSettings.java
+++ src/com/stag/horns/fragments/NotificationSettings.java
@@ -4,6 +4,7 @@ import com.android.internal.logging.nano.MetricsProto;
 
 import android.os.Bundle;
 import com.android.settings.R;
+import android.provider.Settings;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
@@ -12,10 +13,17 @@ import androidx.preference.PreferenceScreen;
 import com.android.settings.SettingsPreferenceFragment;
 
 import com.stag.horns.preferences.Utils;
+import com.stag.horns.preferences.SystemSettingMasterSwitchPreference;
 
-public class NotificationSettings extends SettingsPreferenceFragment {
+public class NotificationSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+    private static final String LIGHTS_CATEGORY = "notification_lights";
+    private static final String BATTERY_LIGHT_ENABLED = "battery_light_enabled";
+
+    private PreferenceCategory mLightsCategory;
+    private SystemSettingMasterSwitchPreference mBatteryLightEnabled;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -28,6 +36,27 @@ public class NotificationSettings extends SettingsPreferenceFragment {
         if (!Utils.isVoiceCapable(getActivity())) {
             prefScreen.removePreference(incallVibCategory);
         }
+        mBatteryLightEnabled = (SystemSettingMasterSwitchPreference) findPreference(BATTERY_LIGHT_ENABLED);
+        mBatteryLightEnabled.setOnPreferenceChangeListener(this);
+        int batteryLightEnabled = Settings.System.getInt(getContentResolver(),
+                BATTERY_LIGHT_ENABLED, 1);
+        mBatteryLightEnabled.setChecked(batteryLightEnabled != 0);
+
+        mLightsCategory = (PreferenceCategory) findPreference(LIGHTS_CATEGORY);
+        if (!getResources().getBoolean(com.android.internal.R.bool.config_hasNotificationLed)) {
+            getPreferenceScreen().removePreference(mLightsCategory);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mBatteryLightEnabled) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(),
+		            BATTERY_LIGHT_ENABLED, value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 
     @Override
