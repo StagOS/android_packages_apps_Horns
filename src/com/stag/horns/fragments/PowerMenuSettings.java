fb6b5544acdec0d01766db1eb236d31e047d0b6e
rpingat <pingatravi@gmail.com>
2020-02-20 23:33:28 +0530

    Powermenu Torch option [2/2]
    Change-Id: I286f93f83513cfd864dd7fae7bda18348d8c6aa4

--- src/com/stag/horns/fragments/PowerMenuSettings.java
+++ src/com/stag/horns/fragments/PowerMenuSettings.java
@@ -35,6 +35,7 @@ import androidx.annotation.NonNull;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.SettingsPreferenceFragment;
+import com.stag.horns.preferences.Utils;
 
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -43,6 +44,8 @@ import java.util.List;
 
 public class PowerMenuSettings extends SettingsPreferenceFragment
                 implements Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
 
     private SwitchPreference mPowermenuTorch;
 
@@ -55,11 +58,24 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
 
+        mPowermenuTorch = (SwitchPreference) findPreference(KEY_POWERMENU_TORCH);
+        mPowermenuTorch.setOnPreferenceChangeListener(this);
+        if (!Utils.deviceSupportsFlashLight(getActivity())) {
+            prefScreen.removePreference(mPowermenuTorch);
+        } else {
+        mPowermenuTorch.setChecked((Settings.System.getInt(resolver,
+                Settings.System.POWERMENU_TORCH, 0) == 1));
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-
+        if (preference == mPowermenuTorch) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_TORCH, value ? 1 : 0);
+            return true;
+        }
         return false;
     }
 
