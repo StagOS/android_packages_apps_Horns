a5f08e918945c4ec66cedd7ec1b7e4e07394c64f
phenomenall <venkysaihot@gmail.com>
2020-04-25 09:37:45 +0530

    Revert "Allow to set QS panel columns and rows [2/2]"
    This reverts commit 62c480846f25b092646858b5a873fc821a63fe70.

--- src/com/stag/horns/fragments/QuickSettings.java
+++ src/com/stag/horns/fragments/QuickSettings.java
@@ -41,21 +41,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
     private static final String KEY_QS_PANEL_ALPHA = "qs_panel_alpha";
     private static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
-    private static final String PREF_ROWS_PORTRAIT = "qs_rows_portrait";
-    private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
-    private static final String PREF_COLUMNS_PORTRAIT = "qs_columns_portrait";
-    private static final String PREF_COLUMNS_LANDSCAPE = "qs_columns_landscape";
-    private static final String PREF_COLUMNS_QUICKBAR = "qs_columns_quickbar";
-    private static final String FOOTER_TEXT_STRING = "footer_text_string";
-
     private SystemSettingEditTextPreference mFooterString;
     private CustomSeekBarPreference mQsPanelAlpha;
     private SystemSettingMasterSwitchPreference mCustomHeader;
-    private SystemSettingSeekBarPreference mRowsPortrait;
-    private SystemSettingSeekBarPreference mRowsLandscape;
-    private SystemSettingSeekBarPreference mQsColumnsPortrait;
-    private SystemSettingSeekBarPreference mQsColumnsLandscape;
-    private SystemSettingSeekBarPreference mQsColumnsQuickbar;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -75,36 +63,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 Settings.System.STATUS_BAR_CUSTOM_HEADER, 0);
         mCustomHeader.setChecked(qsHeader != 0);
         mCustomHeader.setOnPreferenceChangeListener(this);
-
-        mQsColumnsQuickbar = (SystemSettingSeekBarPreference) findPreference(PREF_COLUMNS_QUICKBAR);
-        int columnsQuickbar = Settings.System.getInt(resolver,
-                Settings.System.OMNI_QS_QUICKBAR_COLUMNS, 6);
-        mQsColumnsQuickbar.setValue(columnsQuickbar);
-        mQsColumnsQuickbar.setOnPreferenceChangeListener(this);
-
-        mRowsPortrait = (SystemSettingSeekBarPreference) findPreference(PREF_ROWS_PORTRAIT);
-        int rowsPortrait = Settings.System.getIntForUser(resolver,
-                Settings.System.OMNI_QS_LAYOUT_ROWS, 3, UserHandle.USER_CURRENT);
-        mRowsPortrait.setValue(rowsPortrait);
-        mRowsPortrait.setOnPreferenceChangeListener(this);
-
-        mRowsLandscape = (SystemSettingSeekBarPreference) findPreference(PREF_ROWS_LANDSCAPE);
-        int rowsLandscape = Settings.System.getIntForUser(resolver,
-                Settings.System.OMNI_QS_LAYOUT_ROWS_LANDSCAPE, 2, UserHandle.USER_CURRENT);
-        mRowsLandscape.setValue(rowsLandscape);
-        mRowsLandscape.setOnPreferenceChangeListener(this);
-
-        mQsColumnsPortrait = (SystemSettingSeekBarPreference) findPreference(PREF_COLUMNS_PORTRAIT);
-        int columnsPortrait = Settings.System.getIntForUser(resolver,
-                Settings.System.OMNI_QS_LAYOUT_COLUMNS, 3, UserHandle.USER_CURRENT);
-        mQsColumnsPortrait.setValue(columnsPortrait);
-        mQsColumnsPortrait.setOnPreferenceChangeListener(this);
-
-        mQsColumnsLandscape = (SystemSettingSeekBarPreference) findPreference(PREF_COLUMNS_LANDSCAPE);
-        int columnsLandscape = Settings.System.getIntForUser(resolver,
-                Settings.System.OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE, 4, UserHandle.USER_CURRENT);
-        mQsColumnsLandscape.setValue(columnsLandscape);
-        mQsColumnsLandscape.setOnPreferenceChangeListener(this);
 
         mFooterString = (SystemSettingEditTextPreference) findPreference(FOOTER_TEXT_STRING);
         mFooterString.setOnPreferenceChangeListener(this);
@@ -133,37 +91,39 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(resolver,
                     Settings.System.STATUS_BAR_CUSTOM_HEADER, header ? 1 : 0);
             return true;
-        } else if (preference == mQsColumnsQuickbar) {
-            int value = (Integer) newValue;
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.OMNI_QS_QUICKBAR_COLUMNS, value, UserHandle.USER_CURRENT);
-            return true;
-        } else if (preference == mRowsPortrait) {
-            int value = (Integer) newValue;
-            Settings.System.putIntForUser(resolver,
-                    Settings.System.OMNI_QS_LAYOUT_ROWS, value, UserHandle.USER_CURRENT);
-            return true;
-        } else if (preference == mRowsLandscape) {
-            int value = (Integer) newValue;
-            Settings.System.putIntForUser(resolver,
-                    Settings.System.OMNI_QS_LAYOUT_ROWS_LANDSCAPE, value, UserHandle.USER_CURRENT);
-            return true;
-        } else if (preference == mQsColumnsPortrait) {
-            int value = (Integer) newValue;
-            Settings.System.putIntForUser(resolver,
-                    Settings.System.OMNI_QS_LAYOUT_COLUMNS, value, UserHandle.USER_CURRENT);
-            return true;
-        } else if (preference == mQsColumnsLandscape) {
-            int value = (Integer) newValue;
-            Settings.System.putIntForUser(resolver,
-                    Settings.System.OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE, value, UserHandle.USER_CURRENT);
-            return true;
 	} else if (preference == mFooterString) {
             String value = (String) newValue;
             if (TextUtils.isEmpty(value) || value == null) {
                 mFooterString.setText("#LetsAIMify");
                 Settings.System.putString(getActivity().getContentResolver(),
                         Settings.System.FOOTER_TEXT_STRING, "#LetsAIMify");
+	}
+        return false;
+    }
+
+    private void updateTileAnimationStyleSummary(int tileAnimationStyle) {
+        String prefix = (String) mTileAnimationStyle.getEntries()[mTileAnimationStyle.findIndexOfValue(String
+                .valueOf(tileAnimationStyle))];
+        mTileAnimationStyle.setSummary(getResources().getString(R.string.qs_set_animation_style, prefix));
+    }
+
+    private void updateTileAnimationDurationSummary(int tileAnimationDuration) {
+        String prefix = (String) mTileAnimationDuration.getEntries()[mTileAnimationDuration.findIndexOfValue(String
+                .valueOf(tileAnimationDuration))];
+        mTileAnimationDuration.setSummary(getResources().getString(R.string.qs_set_animation_duration, prefix));
+    }
+
+    private void updateTileAnimationInterpolatorSummary(int tileAnimationInterpolator) {
+        String prefix = (String) mTileAnimationInterpolator.getEntries()[mTileAnimationInterpolator.findIndexOfValue(String
+                .valueOf(tileAnimationInterpolator))];
+        mTileAnimationInterpolator.setSummary(getResources().getString(R.string.qs_set_animation_interpolator, prefix));
+    }
+
+    private void updateAnimTileStyle(int tileAnimationStyle) {
+        if (mTileAnimationDuration != null) {
+            if (tileAnimationStyle == 0) {
+                mTileAnimationDuration.setSelectable(false);
+                mTileAnimationInterpolator.setSelectable(false);
             } else {
                 Settings.System.putString(getActivity().getContentResolver(),
                         Settings.System.FOOTER_TEXT_STRING, value);
