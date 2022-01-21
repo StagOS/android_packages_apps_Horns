834f249132a2735b0c00198e4d25865249832237
SagarMakhar <sagarmakhar@gmail.com>
2022-01-21 09:39:38 +0000

    Bring back UDFPS icons and Animations [2/3]
    * FOD => UDFPS. Make everything consistent.
    * Convert Fod_tweaks to UDFPS Settings
    Change-Id: Ie9ffd199ddf0584045de7e7bbdeda2b71307a790
    
    Change-Id: I5c2b8a93a6921226e9891ad3205edde1c6eb2011

--- src/com/stag/horns/fragments/SystemSettings.java
+++ src/com/stag/horns/fragments/SystemSettings.java
@@ -39,7 +39,7 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
 
-import com.android.internal.util.custom.udfps.UdfpsUtils;
+import com.android.internal.util.stag.udfps.UdfpsUtils;
 
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 import com.stag.horns.preferences.Utils;
@@ -54,7 +54,7 @@ public class SystemSettings extends SettingsPreferenceFragment implements
 	private static final String TAG = "SystemSettings";
 
 	// private ListPreference mCustomTheme;
-        private static final String FOD_TWEAKS = "fod_tweaks";
+        private static final String UDFPS_SETTINGS = "udfps_settings";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -72,7 +72,7 @@ public class SystemSettings extends SettingsPreferenceFragment implements
 		// mCustomTheme.setOnPreferenceChangeListener(this);
 		// Log.d(TAG, "Current theme: " + String.valueOf(currentTheme));
          if (!UdfpsUtils.hasUdfpsSupport(getContext())) {
-             prefScreen.removePreference(findPreference(FOD_TWEAKS));
+             prefScreen.removePreference(findPreference(UDFPS_SETTINGS));
          }
     }
 
