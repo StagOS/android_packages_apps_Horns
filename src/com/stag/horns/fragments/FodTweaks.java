syberia-project/platform_packages_apps_SyberiaSettings

Create FOD tweaks fragment
and hide it on non-FOD devices

Signed-off-by: DennySPb <dennyspb@gmail.com>
Change-Id: Ic652bcadcaa89c1cee7bba8fdb15ccaff4ad06af

src/com/syberia/settings/fragments/FodTweaks.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright © 2018-2020 Syberia Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.syberia.settings.fragments;
+
+import android.os.Bundle;
+import android.content.Context;
+import com.android.settings.R;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+public class FodTweaks extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.fod_tweaks);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.SYBERIA;
+    }
+
+}

StagOS/android_packages_apps_Horns

src/com/stag/horns/fragments/FodTweaks.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright © 2018-2020 Syberia Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.stag.horns.fragments;
+
+import android.os.Bundle;
+import android.content.Context;
+import com.android.settings.R;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+public class FodTweaks extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.fod_tweaks);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.HORNS;
+    }
+
+}