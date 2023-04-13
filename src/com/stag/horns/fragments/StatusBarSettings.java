2030eb70d1153940655163bab8c7f0e5dcdd8652
vjspranav <pranavasri@live.in>
2023-04-13 21:31:50 +0000

    [TEMP] Revert "Add battery styles and customizations for Android 12 [2/2]"
    This reverts commit 5a4966e937578813ec057fdc59963e8f655aa545.

--- src/com/stag/horns/fragments/StatusBarSettings.java
+++ src/com/stag/horns/fragments/StatusBarSettings.java
@@ -64,18 +64,8 @@ import com.stag.horns.preferences.SystemSettingMasterSwitchPreference;
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener, Indexable {
 
-    private static final String PREF_STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
-    private static final String PREF_STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
-	private static final String KEY_NETWORK_TRAFFIC = "network_traffic_state";
+    private static final String KEY_NETWORK_TRAFFIC = "network_traffic_state";
 
-    private static final int BATTERY_STYLE_PORTRAIT = 0;
-    private static final int BATTERY_STYLE_TEXT = 4;
-    private static final int BATTERY_STYLE_HIDDEN = 5;
-    private static final int BATTERY_PERCENT_HIDDEN = 0;
-
-    private ListPreference mBatteryPercent;
-    private ListPreference mBatteryStyle;
-    private int mBatteryPercentValue;
     private SystemSettingMasterSwitchPreference mNetworkTraffic;
 
     @Override
@@ -84,26 +74,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
 		addPreferencesFromResource(R.xml.horns_statusbar);
         ContentResolver resolver = getActivity().getContentResolver();
 
-        int batterystyle = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
-
-        mBatteryStyle = (ListPreference) findPreference(PREF_STATUS_BAR_BATTERY_STYLE);
-        mBatteryStyle.setValue(String.valueOf(batterystyle));
-        mBatteryStyle.setSummary(mBatteryStyle.getEntry());
-        mBatteryStyle.setOnPreferenceChangeListener(this);
-
-        mBatteryPercentValue = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, BATTERY_PERCENT_HIDDEN, UserHandle.USER_CURRENT);
-        mBatteryPercent = (ListPreference) findPreference(PREF_STATUS_BAR_SHOW_BATTERY_PERCENT);
-        mBatteryPercent.setValue(String.valueOf(mBatteryPercentValue));
-        mBatteryPercent.setSummary(mBatteryPercent.getEntry());
-        mBatteryPercent.setOnPreferenceChangeListener(this);
-
-        mBatteryPercent.setEnabled(
-                batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN);
-
-		mNetworkTraffic = (SystemSettingMasterSwitchPreference)
-                findPreference(KEY_NETWORK_TRAFFIC);
+	mNetworkTraffic = (SystemSettingMasterSwitchPreference) findPreference(KEY_NETWORK_TRAFFIC);
         boolean enabled = Settings.System.getIntForUser(resolver,
                 KEY_NETWORK_TRAFFIC, 0, UserHandle.USER_CURRENT) == 1;
         mNetworkTraffic.setChecked(enabled);
@@ -113,25 +84,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
 		ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mBatteryStyle) {
-            int batterystyle = Integer.parseInt((String) newValue);
-            Settings.System.putIntForUser(resolver,
-                Settings.System.STATUS_BAR_BATTERY_STYLE, batterystyle,
-                UserHandle.USER_CURRENT);
-            int index = mBatteryStyle.findIndexOfValue((String) newValue);
-            mBatteryStyle.setSummary(mBatteryStyle.getEntries()[index]);
-            mBatteryPercent.setEnabled(
-                    batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN);
-            return true;
-        } else if (preference == mBatteryPercent) {
-            mBatteryPercentValue = Integer.parseInt((String) newValue);
-            Settings.System.putIntForUser(resolver,
-                    Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, mBatteryPercentValue,
-                    UserHandle.USER_CURRENT);
-            int index = mBatteryPercent.findIndexOfValue((String) newValue);
-            mBatteryPercent.setSummary(mBatteryPercent.getEntries()[index]);
-            return true;
-        } else if (preference == mNetworkTraffic) {
+        if (preference == mNetworkTraffic) {
             boolean value = (Boolean) newValue;
             Settings.System.putIntForUser(resolver, KEY_NETWORK_TRAFFIC,
                     value ? 1 : 0, UserHandle.USER_CURRENT);
